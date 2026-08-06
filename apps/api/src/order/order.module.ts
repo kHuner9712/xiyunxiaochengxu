cef82ff610605a8a8db414bd51290b10eb65bc49
盛鑫 <shengxin9712@gmail.com>
2026-08-06 17:53:59 +0800

    fix(order): break promotion module import cycle

--- apps/api/src/order/order.module.ts
+++ apps/api/src/order/order.module.ts
@@ -1,4 +1,4 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { WeappOrderController, AdminOrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { TransactionalOrderService } from './transactional-order.service';
@@ -6,17 +6,9 @@ import { PromotionCheckoutService } from './promotion-checkout.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { BusinessEventModule } from '../common/business-event.module';
 import { BenefitPackageModule } from '../benefit-package/benefit-package.module';
-import { GroupBuyModule } from '../group-buy/group-buy.module';
-import { FlashSaleModule } from '../flash-sale/flash-sale.module';
 
 @Module({
-  imports: [
-    PrismaModule,
-    BusinessEventModule,
-    BenefitPackageModule,
-    forwardRef(() => GroupBuyModule),
-    forwardRef(() => FlashSaleModule),
-  ],
+  imports: [PrismaModule, BusinessEventModule, BenefitPackageModule],
   controllers: [WeappOrderController, AdminOrderController],
   providers: [
     PromotionCheckoutService,
