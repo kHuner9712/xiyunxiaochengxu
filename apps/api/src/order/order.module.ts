f59db7c38896781aa77fe896336d5cd46bd5ffe1
盛鑫 <shengxin9712@gmail.com>
2026-08-06 18:03:01 +0800

    revert(order): restore promotion module wiring after DI diagnosis

--- apps/api/src/order/order.module.ts
+++ apps/api/src/order/order.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { WeappOrderController, AdminOrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { TransactionalOrderService } from './transactional-order.service';
@@ -6,9 +6,17 @@ import { PromotionCheckoutService } from './promotion-checkout.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { BusinessEventModule } from '../common/business-event.module';
 import { BenefitPackageModule } from '../benefit-package/benefit-package.module';
+import { GroupBuyModule } from '../group-buy/group-buy.module';
+import { FlashSaleModule } from '../flash-sale/flash-sale.module';
 
 @Module({
-  imports: [PrismaModule, BusinessEventModule, BenefitPackageModule],
+  imports: [
+    PrismaModule,
+    BusinessEventModule,
+    BenefitPackageModule,
+    forwardRef(() => GroupBuyModule),
+    forwardRef(() => FlashSaleModule),
+  ],
   controllers: [WeappOrderController, AdminOrderController],
   providers: [
     PromotionCheckoutService,
