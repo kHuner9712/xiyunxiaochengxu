103ad5ebd3797b5c726aee5763582382c30619ee
盛鑫 <shengxin9712@gmail.com>
2026-08-06 15:05:33 +0800

    feat(flash-sale): activate transactional implementation

--- apps/api/src/flash-sale/flash-sale.module.ts
+++ apps/api/src/flash-sale/flash-sale.module.ts
@@ -1,13 +1,23 @@
 import { Module, forwardRef } from '@nestjs/common';
-import { AdminFlashSaleController, WeappFlashSaleController } from './flash-sale.controller';
+import {
+  AdminFlashSaleController,
+  WeappFlashSaleController,
+} from './flash-sale.controller';
 import { FlashSaleService } from './flash-sale.service';
+import { TransactionalFlashSaleService } from './transactional-flash-sale.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { OrderModule } from '../order/order.module';
 
 @Module({
   imports: [PrismaModule, forwardRef(() => OrderModule)],
   controllers: [AdminFlashSaleController, WeappFlashSaleController],
-  providers: [FlashSaleService],
+  providers: [
+    TransactionalFlashSaleService,
+    {
+      provide: FlashSaleService,
+      useExisting: TransactionalFlashSaleService,
+    },
+  ],
   exports: [FlashSaleService],
 })
 export class FlashSaleModule {}
