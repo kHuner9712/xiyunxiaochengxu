ca6cac34e20207071ec5dbbe74696260bad64162
盛鑫 <shengxin9712@gmail.com>
2026-08-06 15:05:19 +0800

    feat(order): export transactional promotion checkout

--- apps/api/src/order/order.module.ts
+++ apps/api/src/order/order.module.ts
@@ -1,15 +1,21 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { WeappOrderController, AdminOrderController } from './order.controller';
 import { OrderService } from './order.service';
+import { PromotionCheckoutService } from './promotion-checkout.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { BenefitPackageModule } from '../benefit-package/benefit-package.module';
 import { GroupBuyModule } from '../group-buy/group-buy.module';
 import { FlashSaleModule } from '../flash-sale/flash-sale.module';
 
 @Module({
-  imports: [PrismaModule, BenefitPackageModule, forwardRef(() => GroupBuyModule), forwardRef(() => FlashSaleModule)],
+  imports: [
+    PrismaModule,
+    BenefitPackageModule,
+    forwardRef(() => GroupBuyModule),
+    forwardRef(() => FlashSaleModule),
+  ],
   controllers: [WeappOrderController, AdminOrderController],
-  providers: [OrderService],
-  exports: [OrderService],
+  providers: [OrderService, PromotionCheckoutService],
+  exports: [OrderService, PromotionCheckoutService],
 })
 export class OrderModule {}
