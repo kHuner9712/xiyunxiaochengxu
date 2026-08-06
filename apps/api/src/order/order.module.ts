9a71d9e751b6effb154581a5c60e5310d188326e
盛鑫 <shengxin9712@gmail.com>
2026-08-06 15:36:47 +0800

    fix(order): import business event module explicitly

--- apps/api/src/order/order.module.ts
+++ apps/api/src/order/order.module.ts
@@ -4,6 +4,7 @@ import { OrderService } from './order.service';
 import { TransactionalOrderService } from './transactional-order.service';
 import { PromotionCheckoutService } from './promotion-checkout.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
+import { BusinessEventModule } from '../common/business-event.module';
 import { BenefitPackageModule } from '../benefit-package/benefit-package.module';
 import { GroupBuyModule } from '../group-buy/group-buy.module';
 import { FlashSaleModule } from '../flash-sale/flash-sale.module';
@@ -11,6 +12,7 @@ import { FlashSaleModule } from '../flash-sale/flash-sale.module';
 @Module({
   imports: [
     PrismaModule,
+    BusinessEventModule,
     BenefitPackageModule,
     forwardRef(() => GroupBuyModule),
     forwardRef(() => FlashSaleModule),
