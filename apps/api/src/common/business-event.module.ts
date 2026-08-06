807e6a23b607468cbaa9655a9c75bfead255b1bf
盛鑫 <shengxin9712@gmail.com>
2026-08-06 15:40:51 +0800

    fix(common): make business event module self-contained

--- apps/api/src/common/business-event.module.ts
+++ apps/api/src/common/business-event.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { BusinessEventService } from './business-event.service';
 import { BusinessEventController } from './business-event.controller';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Global()
 @Module({
-  imports: [PrismaModule],
+  imports: [ConfigModule, PrismaModule],
   controllers: [BusinessEventController],
   providers: [BusinessEventService],
   exports: [BusinessEventService],
