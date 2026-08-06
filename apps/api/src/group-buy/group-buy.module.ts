8f64a0c2bd79d0fd14bc1744c0def11aeb654bc2
盛鑫 <shengxin9712@gmail.com>
2026-08-06 15:05:53 +0800

    feat(group-buy): activate transactional implementation

--- apps/api/src/group-buy/group-buy.module.ts
+++ apps/api/src/group-buy/group-buy.module.ts
@@ -1,13 +1,23 @@
 import { Module, forwardRef } from '@nestjs/common';
-import { AdminGroupBuyController, WeappGroupBuyController } from './group-buy.controller';
+import {
+  AdminGroupBuyController,
+  WeappGroupBuyController,
+} from './group-buy.controller';
 import { GroupBuyService } from './group-buy.service';
+import { TransactionalGroupBuyService } from './transactional-group-buy.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { OrderModule } from '../order/order.module';
 
 @Module({
   imports: [PrismaModule, forwardRef(() => OrderModule)],
   controllers: [AdminGroupBuyController, WeappGroupBuyController],
-  providers: [GroupBuyService],
+  providers: [
+    TransactionalGroupBuyService,
+    {
+      provide: GroupBuyService,
+      useExisting: TransactionalGroupBuyService,
+    },
+  ],
   exports: [GroupBuyService],
 })
 export class GroupBuyModule {}
