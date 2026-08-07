b46962c34cb327d455a35bd455de0b196ec2514c
盛鑫 <shengxin9712@gmail.com>
2026-08-07 18:20:38 +0800

    fix: use production referral reward service

--- apps/api/src/share/share.module.ts
+++ apps/api/src/share/share.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { WeappShareController, AdminShareController } from './share.controller';
 import { ShareService } from './share.service';
+import { ProductionShareService } from './production-share.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { RedisModule } from '../common/redis/redis.module';
 import { PointsModule } from '../points/points.module';
@@ -9,7 +10,12 @@ import { CouponModule } from '../coupon/coupon.module';
 @Module({
   imports: [PrismaModule, RedisModule, PointsModule, CouponModule],
   controllers: [WeappShareController, AdminShareController],
-  providers: [ShareService],
+  providers: [
+    {
+      provide: ShareService,
+      useClass: ProductionShareService,
+    },
+  ],
   exports: [ShareService],
 })
 export class ShareModule {}
