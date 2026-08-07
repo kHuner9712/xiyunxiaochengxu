e9df9541af7dd8f421323706d113da0b60f38efb
盛鑫 <shengxin9712@gmail.com>
2026-08-07 19:03:21 +0800

    fix: use production group-buy runtime service

--- apps/api/src/group-buy/group-buy.module.ts
+++ apps/api/src/group-buy/group-buy.module.ts
@@ -4,7 +4,7 @@ import {
   WeappGroupBuyController,
 } from './group-buy.controller';
 import { GroupBuyService } from './group-buy.service';
-import { TransactionalGroupBuyService } from './transactional-group-buy.service';
+import { ProductionGroupBuyService } from './production-group-buy.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { OrderModule } from '../order/order.module';
 import { BenefitPackageModule } from '../benefit-package/benefit-package.module';
@@ -19,7 +19,7 @@ import { BenefitPackageModule } from '../benefit-package/benefit-package.module'
   providers: [
     {
       provide: GroupBuyService,
-      useClass: TransactionalGroupBuyService,
+      useClass: ProductionGroupBuyService,
     },
   ],
   exports: [GroupBuyService],
