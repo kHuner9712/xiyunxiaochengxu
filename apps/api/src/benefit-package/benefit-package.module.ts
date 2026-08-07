3d30ff9b230e1683b06795c9dae34c02e7dcf602
盛鑫 <shengxin9712@gmail.com>
2026-08-07 18:13:35 +0800

    fix: use refund-aware benefit package service

--- apps/api/src/benefit-package/benefit-package.module.ts
+++ apps/api/src/benefit-package/benefit-package.module.ts
@@ -4,13 +4,19 @@ import {
   AdminBenefitPackageController,
 } from './benefit-package.controller';
 import { BenefitPackageService } from './benefit-package.service';
+import { ProductionBenefitPackageService } from './production-benefit-package.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { MerchantSettlementModule } from '../merchant-settlement/merchant-settlement.module';
 
 @Module({
   imports: [PrismaModule, forwardRef(() => MerchantSettlementModule)],
   controllers: [WeappBenefitPackageController, AdminBenefitPackageController],
-  providers: [BenefitPackageService],
+  providers: [
+    {
+      provide: BenefitPackageService,
+      useClass: ProductionBenefitPackageService,
+    },
+  ],
   exports: [BenefitPackageService],
 })
 export class BenefitPackageModule {}
