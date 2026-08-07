8d49205da1a69a4a5fe8b3d187c3321311b3d395
盛鑫 <shengxin9712@gmail.com>
2026-08-08 03:03:35 +0800

    fix: use time-aware home service

--- apps/api/src/home/home.module.ts
+++ apps/api/src/home/home.module.ts
@@ -3,12 +3,13 @@ import { WeappHomeController } from './home.controller';
 import { AdminBannerController } from './admin-banner.controller';
 import { AdminHomeDecorController } from './admin-home-decor.controller';
 import { HomeService } from './home.service';
+import { ProductionHomeService } from './production-home.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [WeappHomeController, AdminBannerController, AdminHomeDecorController],
-  providers: [HomeService],
+  providers: [{ provide: HomeService, useClass: ProductionHomeService }],
   exports: [HomeService],
 })
 export class HomeModule {}
