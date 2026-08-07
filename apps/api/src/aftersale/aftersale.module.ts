87c9ab6da65fcc57fe105d8716f9c33a33e7cf4e
盛鑫 <shengxin9712@gmail.com>
2026-08-07 20:47:36 +0800

    fix: use stock-safe aftersale review service

--- apps/api/src/aftersale/aftersale.module.ts
+++ apps/api/src/aftersale/aftersale.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { WeappAftersaleController, AdminAftersaleController } from './aftersale.controller';
 import { AftersaleService } from './aftersale.service';
+import { ProductionAftersaleService } from './production-aftersale.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { PaymentModule } from '../payment/payment.module';
 
 @Module({
   imports: [PrismaModule, PaymentModule],
   controllers: [WeappAftersaleController, AdminAftersaleController],
-  providers: [AftersaleService],
+  providers: [
+    {
+      provide: AftersaleService,
+      useClass: ProductionAftersaleService,
+    },
+  ],
   exports: [AftersaleService],
 })
 export class AftersaleModule {}
