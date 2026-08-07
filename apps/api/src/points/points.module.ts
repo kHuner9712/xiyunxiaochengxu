a435f6446d1984cbbc58eb60f5ae205e1046f76e
盛鑫 <shengxin9712@gmail.com>
2026-08-08 02:52:39 +0800

    fix: bind fifo-safe points service

--- apps/api/src/points/points.module.ts
+++ apps/api/src/points/points.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { WeappPointsController, AdminPointsController } from './points.controller';
 import { PointsService } from './points.service';
+import { ProductionPointsService } from './production-points.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { RedisModule } from '../common/redis/redis.module';
 
 @Module({
   imports: [PrismaModule, RedisModule],
   controllers: [WeappPointsController, AdminPointsController],
-  providers: [PointsService],
+  providers: [
+    { provide: PointsService, useClass: ProductionPointsService },
+  ],
   exports: [PointsService],
 })
 export class PointsModule {}
