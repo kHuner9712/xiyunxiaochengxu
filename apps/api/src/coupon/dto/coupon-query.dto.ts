f2d2a0847b16f2e52a6d130b9c0c2c8281f809a7
盛鑫 <shengxin9712@gmail.com>
2026-08-08 01:08:13 +0800

    fix: validate coupon list and usable queries

--- apps/api/src/coupon/dto/coupon-query.dto.ts
+++ apps/api/src/coupon/dto/coupon-query.dto.ts
@@ -1,4 +1,4 @@
-import { IsOptional, IsInt, IsString } from 'class-validator';
+import { IsIn, IsInt, IsOptional, IsString, Max, Min } from 'class-validator';
 import { Type } from 'class-transformer';
 import { PaginationDto } from '../../common/dto/pagination.dto';
 
@@ -6,14 +6,47 @@ export class CouponQueryDto extends PaginationDto {
   @IsOptional()
   @Type(() => Number)
   @IsInt()
+  @IsIn([1, 2, 3, 4])
   type?: number;
 
   @IsOptional()
   @Type(() => Number)
   @IsInt()
+  @IsIn([0, 1])
   status?: number;
 
   @IsOptional()
   @IsString()
   name?: string;
+}
+
+export class CouponListQueryDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  pageSize = 20;
+}
+
+export class UserCouponListQueryDto extends CouponListQueryDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @IsIn([1, 2, 3, 4])
+  userCouponStatus?: number;
+}
+
+export class UsableCouponQueryDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(0)
+  amount = 0;
 }
