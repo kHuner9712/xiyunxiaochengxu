47f8756f37c4546372cdeb53ba59afd4d9856554
盛鑫 <shengxin9712@gmail.com>
2026-08-08 09:59:52 +0800

    chore: import system config for order runtime

--- apps/api/src/order/order.module.ts
+++ apps/api/src/order/order.module.ts
@@ -8,12 +8,14 @@ import { BusinessEventModule } from '../common/business-event.module';
 import { BenefitPackageModule } from '../benefit-package/benefit-package.module';
 import { GroupBuyModule } from '../group-buy/group-buy.module';
 import { FlashSaleModule } from '../flash-sale/flash-sale.module';
+import { SystemConfigModule } from '../system-config/system-config.module';
 
 @Module({
   imports: [
     PrismaModule,
     BusinessEventModule,
     BenefitPackageModule,
+    SystemConfigModule,
     forwardRef(() => GroupBuyModule),
     forwardRef(() => FlashSaleModule),
   ],
