730f8d1166483fae711df6b45fe5d8aac6c1acdb
盛鑫 <shengxin9712@gmail.com>
2026-08-08 10:15:12 +0800

    feat: bind executable activity checkout services

--- apps/api/src/activity/activity.module.ts
+++ apps/api/src/activity/activity.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { WeappActivityController, AdminActivityController } from './activity.controller';
 import { ActivityService } from './activity.service';
-import { ProductionActivityService } from './production-activity.service';
+import { CheckoutReadyProductionActivityService } from './checkout-ready-production-activity.service';
+import { ActivityCheckoutService } from './activity-checkout.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { ContentModule } from '../content/content.module';
+import { OrderModule } from '../order/order.module';
+import { SystemConfigModule } from '../system-config/system-config.module';
 
 @Module({
-  imports: [PrismaModule, ContentModule],
+  imports: [PrismaModule, ContentModule, OrderModule, SystemConfigModule],
   controllers: [WeappActivityController, AdminActivityController],
-  providers: [{ provide: ActivityService, useClass: ProductionActivityService }],
-  exports: [ActivityService],
+  providers: [
+    ActivityCheckoutService,
+    { provide: ActivityService, useClass: CheckoutReadyProductionActivityService },
+  ],
+  exports: [ActivityService, ActivityCheckoutService],
 })
 export class ActivityModule {}
