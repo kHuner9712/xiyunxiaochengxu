6f116e613ede4545e4f422aae99e5c5105da94b8
盛鑫 <shengxin9712@gmail.com>
2026-08-08 10:31:06 +0800

    fix: bind runtime-configured points provider

--- apps/api/src/points/points.module.ts
+++ apps/api/src/points/points.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { WeappPointsController, AdminPointsController } from './points.controller';
 import { PointsService } from './points.service';
-import { ProductionPointsService } from './production-points.service';
+import { RuntimeConfiguredPointsService } from './runtime-configured-points.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { RedisModule } from '../common/redis/redis.module';
+import { SystemConfigModule } from '../system-config/system-config.module';
 
 @Module({
-  imports: [PrismaModule, RedisModule],
+  imports: [PrismaModule, RedisModule, SystemConfigModule],
   controllers: [WeappPointsController, AdminPointsController],
   providers: [
-    { provide: PointsService, useClass: ProductionPointsService },
+    { provide: PointsService, useClass: RuntimeConfiguredPointsService },
   ],
   exports: [PointsService],
 })
