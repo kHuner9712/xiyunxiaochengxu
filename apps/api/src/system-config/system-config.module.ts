561d290423c47a1195d8f6192c40261bee840ca1
盛鑫 <shengxin9712@gmail.com>
2026-08-08 10:45:38 +0800

    fix: bind resilient system config provider

--- apps/api/src/system-config/system-config.module.ts
+++ apps/api/src/system-config/system-config.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SystemConfigController, WeappCustomerServiceController, AdminCustomerServiceController } from './system-config.controller';
 import { SystemConfigService } from './system-config.service';
+import { ResilientSystemConfigService } from './resilient-system-config.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { RedisModule } from '../common/redis/redis.module';
 
 @Module({
   imports: [PrismaModule, RedisModule],
   controllers: [SystemConfigController, WeappCustomerServiceController, AdminCustomerServiceController],
-  providers: [SystemConfigService],
+  providers: [
+    { provide: SystemConfigService, useClass: ResilientSystemConfigService },
+  ],
   exports: [SystemConfigService],
 })
 export class SystemConfigModule {}
