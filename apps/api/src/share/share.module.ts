12358c9d4f25ec630493bea3923ef8a6492698c0
盛鑫 <shengxin9712@gmail.com>
2026-08-08 10:08:21 +0800

    feat: bind durable first-paid reward provider

--- apps/api/src/share/share.module.ts
+++ apps/api/src/share/share.module.ts
@@ -1,19 +1,20 @@
 import { Module } from '@nestjs/common';
 import { WeappShareController, AdminShareController } from './share.controller';
 import { ShareService } from './share.service';
-import { ProductionShareService } from './production-share.service';
+import { DurableRewardProductionShareService } from './durable-reward-production-share.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { RedisModule } from '../common/redis/redis.module';
 import { PointsModule } from '../points/points.module';
 import { CouponModule } from '../coupon/coupon.module';
+import { SystemConfigModule } from '../system-config/system-config.module';
 
 @Module({
-  imports: [PrismaModule, RedisModule, PointsModule, CouponModule],
+  imports: [PrismaModule, RedisModule, PointsModule, CouponModule, SystemConfigModule],
   controllers: [WeappShareController, AdminShareController],
   providers: [
     {
       provide: ShareService,
-      useClass: ProductionShareService,
+      useClass: DurableRewardProductionShareService,
     },
   ],
   exports: [ShareService],
