4bab17d1503fe64c5b2c4a41883a1f84305380c7
盛鑫 <shengxin9712@gmail.com>
2026-08-09 02:04:21 +0800

    fix: bind authoritative coupon reporting provider

--- apps/api/src/coupon/coupon.module.ts
+++ apps/api/src/coupon/coupon.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { WeappCouponController, AdminCouponController } from './coupon.controller';
 import { CouponService } from './coupon.service';
-import { ReferentiallySafeCouponService } from './referentially-safe-coupon.service';
+import { AuthoritativeCouponReportingService } from './authoritative-coupon-reporting.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [WeappCouponController, AdminCouponController],
   providers: [
-    { provide: CouponService, useClass: ReferentiallySafeCouponService },
+    { provide: CouponService, useClass: AuthoritativeCouponReportingService },
   ],
   exports: [CouponService],
 })
