d502b4cdf8e4ffd7765002bf9ccfa38d0b869404
盛鑫 <shengxin9712@gmail.com>
2026-08-10 01:36:58 +0800

    fix: provide redis for home decor cache invalidation

--- apps/api/src/home/home.module.ts
+++ apps/api/src/home/home.module.ts
@@ -5,9 +5,10 @@ import { AdminHomeDecorController } from './admin-home-decor.controller';
 import { HomeService } from './home.service';
 import { ProductionHomeService } from './production-home.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
+import { RedisModule } from '../common/redis/redis.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, RedisModule],
   controllers: [WeappHomeController, AdminBannerController, AdminHomeDecorController],
   providers: [{ provide: HomeService, useClass: ProductionHomeService }],
   exports: [HomeService],
