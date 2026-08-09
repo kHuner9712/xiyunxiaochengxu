7744ab564d6474549cd4a18f61c10b822a1a5026
盛鑫 <shengxin9712@gmail.com>
2026-08-10 00:52:56 +0800

    feat: add recommendation candidate query

--- apps/api/src/recommendation/dto/recommendation-query.dto.ts
+++ apps/api/src/recommendation/dto/recommendation-query.dto.ts
@@ -1,3 +1,11 @@
+import { IsOptional, IsString, MaxLength } from 'class-validator';
 import { PaginationDto } from '../../common/dto/pagination.dto';
 
 export class RecommendationQueryDto extends PaginationDto {}
+
+export class RecommendationCandidateQueryDto extends PaginationDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  keyword?: string;
+}
