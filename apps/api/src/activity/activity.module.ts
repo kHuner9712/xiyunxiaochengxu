1cbf8c749189bc105f4065187743deb338b34d16
盛鑫 <shengxin9712@gmail.com>
2026-08-09 17:43:34 +0800

    fix: activate new-user concurrency guard in production DI

--- apps/api/src/activity/activity.module.ts
+++ apps/api/src/activity/activity.module.ts
@@ -4,6 +4,7 @@ import { ActivityService } from './activity.service';
 import { CheckoutReadyProductionActivityService } from './checkout-ready-production-activity.service';
 import { ActivityCheckoutService } from './activity-checkout.service';
 import { ActivityMultiItemCheckoutService } from './activity-multi-item-checkout.service';
+import { ExclusiveNewUserActivityCheckoutService } from './exclusive-new-user-activity-checkout.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { ContentModule } from '../content/content.module';
 import { OrderModule } from '../order/order.module';
@@ -13,8 +14,8 @@ import { SystemConfigModule } from '../system-config/system-config.module';
   imports: [PrismaModule, ContentModule, OrderModule, SystemConfigModule],
   controllers: [WeappActivityController, AdminActivityController],
   providers: [
-    ActivityCheckoutService,
     ActivityMultiItemCheckoutService,
+    { provide: ActivityCheckoutService, useClass: ExclusiveNewUserActivityCheckoutService },
     { provide: ActivityService, useClass: CheckoutReadyProductionActivityService },
   ],
   exports: [ActivityService, ActivityCheckoutService],
