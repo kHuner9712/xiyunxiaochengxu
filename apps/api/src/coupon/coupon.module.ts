fe110b699e440bdbe3df9de4a59d961283bae629
盛鑫 <shengxin9712@gmail.com>
2026-08-11 00:52:05 +0800

    feat: activate growth-aware coupon provider

--- apps/api/src/coupon/coupon.module.ts
+++ apps/api/src/coupon/coupon.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { WeappCouponController, AdminCouponController } from './coupon.controller';
 import { CouponService } from './coupon.service';
-import { AuthoritativeCouponReportingService } from './authoritative-coupon-reporting.service';
+import { GrowthAwareCouponService } from './growth-aware-coupon.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [WeappCouponController, AdminCouponController],
   providers: [
-    { provide: CouponService, useClass: AuthoritativeCouponReportingService },
+    { provide: CouponService, useClass: GrowthAwareCouponService },
   ],
   exports: [CouponService],
 })
