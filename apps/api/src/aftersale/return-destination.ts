2185958fe5b7675bcdc2f8e1e45fd4f57a11190d
盛鑫 <shengxin9712@gmail.com>
2026-08-10 22:54:10 +0800

    refactor: isolate return destination contract

---
+++ apps/api/src/aftersale/return-destination.ts
@@ -0,0 +1,8 @@
+export type ReturnDestinationSnapshot = {
+  version: 1;
+  receiverName: string;
+  receiverPhone: string;
+  address: string;
+};
+
+export const RETURN_DESTINATION_ACTION = 'return_destination';
