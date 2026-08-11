d72fdea3426d105e50a821d11ba9f82822f3c996
盛鑫 <shengxin9712@gmail.com>
2026-08-11 22:25:37 +0800

    fix: wire explicit user status service

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { WeappUserController, AdminUserController } from './user.controller';
 import { UserService } from './user.service';
 import { ProductionUserService } from './production-user.service';
+import { UserStatusService } from './user-status.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { RedisModule } from '../common/redis/redis.module';
 import { PointsModule } from '../points/points.module';
@@ -11,6 +12,7 @@ import { PointsModule } from '../points/points.module';
   controllers: [WeappUserController, AdminUserController],
   providers: [
     { provide: UserService, useClass: ProductionUserService },
+    UserStatusService,
   ],
   exports: [UserService],
 })
