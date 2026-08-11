8cebf8dcfb716243300d73a21f7696492df9f54b
盛鑫 <shengxin9712@gmail.com>
2026-08-11 22:19:39 +0800

    fix: wire safe admin points adjustment service

--- apps/api/src/points/points.module.ts
+++ apps/api/src/points/points.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { WeappPointsController, AdminPointsController } from './points.controller';
 import { PointsService } from './points.service';
 import { RuntimeConfiguredPointsService } from './runtime-configured-points.service';
+import { AdminPointsAdjustmentService } from './admin-points-adjustment.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { RedisModule } from '../common/redis/redis.module';
 import { SystemConfigModule } from '../system-config/system-config.module';
@@ -11,7 +12,8 @@ import { SystemConfigModule } from '../system-config/system-config.module';
   controllers: [WeappPointsController, AdminPointsController],
   providers: [
     { provide: PointsService, useClass: RuntimeConfiguredPointsService },
+    AdminPointsAdjustmentService,
   ],
-  exports: [PointsService],
+  exports: [PointsService, AdminPointsAdjustmentService],
 })
 export class PointsModule {}
