d504108a3669f63c95bddcf4d45218961169e6e0
盛鑫 <shengxin9712@gmail.com>
2026-08-11 20:23:10 +0800

    fix: wire bigint-safe product service into runtime

--- apps/api/src/product/product.module.ts
+++ apps/api/src/product/product.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { WeappProductController, AdminProductController } from './product.controller';
 import { ProductService } from './product.service';
-import { ProductionProductService } from './production-product.service';
+import { IdSafeProductionProductService } from './id-safe-production-product.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [WeappProductController, AdminProductController],
   providers: [
-    { provide: ProductService, useClass: ProductionProductService },
+    { provide: ProductService, useClass: IdSafeProductionProductService },
   ],
   exports: [ProductService],
 })
