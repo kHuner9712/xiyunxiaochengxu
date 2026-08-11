a5622f40d9b12de7b4827178c934256186322e46
盛鑫 <shengxin9712@gmail.com>
2026-08-11 20:48:58 +0800

    fix: wire idempotent normal checkout into runtime

--- apps/api/src/order/order.module.ts
+++ apps/api/src/order/order.module.ts
@@ -1,7 +1,7 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { WeappOrderController, AdminOrderController } from './order.controller';
 import { OrderService } from './order.service';
-import { AttributionSafeMemberBenefitOrderService } from './attribution-safe-member-benefit-order.service';
+import { IdempotentAttributionSafeMemberBenefitOrderService } from './idempotent-attribution-safe-member-benefit-order.service';
 import { PromotionCheckoutService } from './promotion-checkout.service';
 import { AttributionAwarePromotionCheckoutService } from './attribution-aware-promotion-checkout.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
@@ -28,7 +28,7 @@ import { SystemConfigModule } from '../system-config/system-config.module';
     },
     {
       provide: OrderService,
-      useClass: AttributionSafeMemberBenefitOrderService,
+      useClass: IdempotentAttributionSafeMemberBenefitOrderService,
     },
   ],
   exports: [OrderService, PromotionCheckoutService],
