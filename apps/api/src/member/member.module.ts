ac46b1a99b0e73cd0f3e7c3fd1b702a46d4008b3
盛鑫 <shengxin9712@gmail.com>
2026-08-13 10:04:25 +0800

    fix: wire atomic member-level provider

--- apps/api/src/member/member.module.ts
+++ apps/api/src/member/member.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { WeappMemberController, AdminMemberController } from './member.controller';
 import { MemberService } from './member.service';
+import { AtomicMemberService } from './atomic-member.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [WeappMemberController, AdminMemberController],
-  providers: [MemberService],
+  providers: [
+    {
+      provide: MemberService,
+      useClass: AtomicMemberService,
+    },
+  ],
   exports: [MemberService],
 })
 export class MemberModule {}
