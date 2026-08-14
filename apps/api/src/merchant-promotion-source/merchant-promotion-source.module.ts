1693afbf38c63bdbd602113a505511ae80fe3442
盛鑫 <shengxin9712@gmail.com>
2026-08-14 20:10:36 +0800

    fix: bind identity-safe promotion source provider

--- apps/api/src/merchant-promotion-source/merchant-promotion-source.module.ts
+++ apps/api/src/merchant-promotion-source/merchant-promotion-source.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { AdminMerchantPromotionSourceController } from './merchant-promotion-source.controller';
+import { IdentitySafeMerchantPromotionSourceService } from './identity-safe-merchant-promotion-source.service';
 import { MerchantPromotionSourceService } from './merchant-promotion-source.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AdminMerchantPromotionSourceController],
-  providers: [MerchantPromotionSourceService],
+  providers: [
+    {
+      provide: MerchantPromotionSourceService,
+      useClass: IdentitySafeMerchantPromotionSourceService,
+    },
+  ],
   exports: [MerchantPromotionSourceService],
 })
 export class MerchantPromotionSourceModule {}
