7b16dbb93a77eee8ede224ea564bd0027af9eeb9
盛鑫 <shengxin9712@gmail.com>
2026-08-16 01:33:22 +0800

    fix(api): route aftersale mutations through transition-safe service

--- apps/api/src/aftersale/aftersale.module.ts
+++ apps/api/src/aftersale/aftersale.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { WeappAftersaleController, AdminAftersaleController } from './aftersale.controller';
 import { AftersaleService } from './aftersale.service';
-import { ReturnDestinationViewAftersaleService } from './return-destination-view-aftersale.service';
 import { AftersaleReviewService } from './aftersale-review.service';
+import { TransitionSafeReturnDestinationAftersaleService } from './transition-safe-return-destination-aftersale.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { PaymentModule } from '../payment/payment.module';
 import { SystemConfigModule } from '../system-config/system-config.module';
@@ -13,7 +13,7 @@ import { SystemConfigModule } from '../system-config/system-config.module';
   providers: [
     {
       provide: AftersaleService,
-      useClass: ReturnDestinationViewAftersaleService,
+      useClass: TransitionSafeReturnDestinationAftersaleService,
     },
     AftersaleReviewService,
   ],
